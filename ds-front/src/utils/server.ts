166f9d02605cfd86a5925f2d8fbdf72d959194f5
thegestalt <lordweasel6@gmail.com>
2022-10-12 12:54:04 -0600

    added dream overview

--- ds-front/src/utils/server.ts
+++ ds-front/src/utils/server.ts
@@ -1,4 +1,4 @@
 export const server = {
-  baseURL: "http://localhost:3000",
-  // baseURL: process.env.VUE_APP_SERVER_ADDRESS,
+  // baseURL: "http://localhost:3000",
+  baseURL: process.env.VUE_APP_SERVER_ADDRESS,
 };
