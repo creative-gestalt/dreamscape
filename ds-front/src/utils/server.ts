1470368a58a5275f90f20c10e6b8f75c3ea2fea7
thegestalt <lordweasel6@gmail.com>
2022-10-13 11:31:17 -0600

    reverter addresses.

--- ds-front/src/utils/server.ts
+++ ds-front/src/utils/server.ts
@@ -1,4 +1,4 @@
 export const server = {
-  baseURL: "http://localhost:3000",
-  // baseURL: process.env.VITE_SERVER_ADDRESS,
+  // baseURL: "http://localhost:3000",
+  baseURL: process.env.VITE_SERVER_ADDRESS,
 };
